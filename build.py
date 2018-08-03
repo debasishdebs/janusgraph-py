40c218cfe8a0e31f696babef5d4b07b34e440115
Debasish Kanhar <dekanhar@in.ibm.com>
2018-08-03 13:39:57 +0530

    Remove source code for TinkerPop's Gremlin-Python. Rather download as PyPi dependency, and import the Class, and Extend it using Inheritence.
    Signed-off-by: Debasish Kanhar <dekanhar@in.ibm.com>

--- build.py
+++ build.py
@@ -2,9 +2,28 @@
 # Emp ID: 05222V
 
 
-from pybuilder.core import use_plugin
+from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
+# the python unittest plugin allows running python's standard library unittests
+use_plugin("python.unittest")
+# this plugin allows installing project dependencies with pip
+use_plugin("python.install_dependencies")
+# a plugin that measures unit test statement coverage
+use_plugin("python.coverage")
+# for packaging purposes since we'll build a tarball
 use_plugin("python.distutils")
 
-default_task = "publish"
+name = "janusgraph-python"
+
+version = "0.0.1"
+
+default_task = ['install_dependencies', 'publish']
+
+# This is an initializer, a block of logic that runs before the project is built.
+@init
+def initialize(project):
+    # Nothing happens here yet, but notice the `project` argument which is automatically injected.
+    project.set_property("coverage_break_build", False)  # default is True
+    project.depends_on("gremlinpython", "==3.3")
+    pass
---
+++ src/main/python/JanusGraphP.py
@@ -0,0 +1,26 @@
+# Name: Debasish Kanhar
+# Emp ID: 05222V
+
+
+from gremlin_python.process.traversal import P
+from gremlin_python import statics
+
+
+class JanusGraphP(P):
+    def __init__(self, operator, value, other=None):
+        self.operator = operator
+        self.value = value
+        self.other = other
+
+        super(JanusGraphP, self).__init__(self.operator, self.value, self.other)
+
+        pass
+
+    @staticmethod
+    def textContains(*args):
+        JanusGraphP("textContains", *args)
+
+
+def textContains(*args):
+    return JanusGraphP.textContains(*args)
+statics.add_static('between', textContains)
