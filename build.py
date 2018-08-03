f5b1c30a10cc961d763b12a19054bcfd56526dd1
Debasish Kanhar <dekanhar@in.ibm.com>
2018-08-03 14:02:41 +0530

    Added Author, licence, tinkerpop_version to build. Dynamically fetch gremlinpython version using Variable and extend it.
    Signed-off-by: Debasish Kanhar <dekanhar@in.ibm.com>

--- build.py
+++ build.py
@@ -2,7 +2,17 @@
 # Emp ID: 05222V
 
 
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, Author
+
+authors = [Author("Debasish Kanhar", "dekanhar@in.ibm.com")]
+description = "Python client drivers for JanusGraph"
+license = "Apache License v2.0"
+
+name = "janusgraph-python"
+
+tinkerpop_version = "3.3.3"
+janusgraph_version = "0.3.0"
+version = "0.0.1"
 
 use_plugin("python.core")
 # the python unittest plugin allows running python's standard library unittests
@@ -14,16 +24,12 @@ use_plugin("python.coverage")
 # for packaging purposes since we'll build a tarball
 use_plugin("python.distutils")
 
-name = "janusgraph-python"
-
-version = "0.0.1"
-
-default_task = ['install_dependencies', 'publish']
+default_task = ['clean', 'install_dependencies', 'publish']
 
 # This is an initializer, a block of logic that runs before the project is built.
 @init
 def initialize(project):
     # Nothing happens here yet, but notice the `project` argument which is automatically injected.
     project.set_property("coverage_break_build", False)  # default is True
-    project.depends_on("gremlinpython", "==3.3")
+    project.depends_on("gremlinpython", "=={}".format(tinkerpop_version))
     pass
