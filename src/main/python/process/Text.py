aa2966facd7795ced21720a65b65d40cf94dab74
Debasish Kanhar <dekanhar@in.ibm.com>
2018-08-06 21:38:23 +0530

    Started adding Text Predicate classes

--- src/main/python/process/Text.py
+++ src/main/python/process/Text.py
@@ -1,2 +1,17 @@
 # Name: Debasish Kanhar
 # Emp ID: 05222V
+
+from gremlin_python.process.traversal import *
+
+
+class Text(object):
+    def __init__(self):
+        pass
+
+    def textContains(self, query):
+        predicate = P("textContains", query)
+        return predicate
+
+    def textFuzzy(self, query):
+        predicate = P("textFuzzy", query)
+        return predicate
