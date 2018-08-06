111616bb388d0ccbc0c4c49194f17ddfe9999bdb
Debasish Kanhar <dekanhar@in.ibm.com>
2018-08-06 21:30:59 +0530

    Starting de-serialization for JanusGraph classes

--- src/main/python/serializer/JanusGraphPSerializer.py
+++ src/main/python/serializer/JanusGraphPSerializer.py
@@ -1,2 +1,10 @@
 # Name: Debasish Kanhar
 # Emp ID: 05222V
+
+
+from gremlin_python.driver.serializer import GraphSONMessageSerializer
+
+
+class JanusGraphPSerializer(object):
+    def __init__(self):
+        pass
