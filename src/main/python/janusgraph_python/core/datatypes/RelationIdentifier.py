4907ed6ad92b36a1c0b0002d5366b957f3fb3b8c
Debasish Kanhar <dekanhar@in.ibm.com>
2018-08-17 14:55:49 +0530

    Added class for RelationIdentifier

---
+++ src/main/python/janusgraph_python/core/datatypes/RelationIdentifier.py
@@ -0,0 +1,36 @@
+# Name: Debasish Kanhar
+
+
+class RelationIdentifier(object):
+    def __init__(self, relationID):
+        self.relationID = relationID
+        pass
+
+    def __eq__(self, other):
+        """
+
+        Args:
+            other (RelationIdentifier):
+
+        Returns:
+
+        """
+        if other is None:
+            return False
+        else:
+            if self.relationID.equals(other.relationID):
+                return True
+            else:
+                return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return self.toString()
+
+    def toString(self):
+        return str(self.relationID)
+
+    def getID(self):
+        return self.toString()
--- src/unittest/python/structure/io/GraphsonReader_test.py
+++ src/unittest/python/structure/io/GraphsonReader_test.py
@@ -1,36 +1,36 @@
-# # Name: Debasish Kanhar
-#
-# import unittest
-# from gremlin_python.structure.io.graphsonV3d0 import GraphSONUtil
-# from janusgraph_python.structure.io.GraphsonReader import JanusGraphSONReader
-#
-#
-# class MockDeserializer(object):
-#     def __init__(self):
-#         pass
-#
-#     @classmethod
-#     def objectify(cls, graphsonObj, reader):
-#
-#         return
-#
-#
-# class TestGraphsonReader(unittest.TestCase):
-#     GRAPHSON_PREFIX = "janusgraph"
-#     GRAPHSON_BASE_TYPE = "MOCK"
-#     GeoShape_GRAPHSON_TYPE = GraphSONUtil.formatType(GRAPHSON_PREFIX, GRAPHSON_BASE_TYPE)
-# 
-#     def setUp(self):
-#         self.readerClass = JanusGraphSONReader()
-#         self.reader = None
-#
-#         pass
-#
-#     def test_mock_deserializer(self):
-#         deserializer = MockDeserializer
-#
-#         self.readerClass.register_deserializer(self.GeoShape_GRAPHSON_TYPE, deserializer)
-#         self.reader = self.readerClass.build()
-#
-#         self.reader.toObject()
-#         return
+# Name: Debasish Kanhar
+
+import unittest
+from gremlin_python.structure.io.graphsonV3d0 import GraphSONUtil
+from janusgraph_python.structure.io.GraphsonReader import JanusGraphSONReader
+
+
+class MockDeserializer(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def objectify(cls, graphsonObj, reader):
+
+        return
+
+
+class TestGraphsonReader(unittest.TestCase):
+    GRAPHSON_PREFIX = "janusgraph"
+    GRAPHSON_BASE_TYPE = "MOCK"
+    GeoShape_GRAPHSON_TYPE = GraphSONUtil.formatType(GRAPHSON_PREFIX, GRAPHSON_BASE_TYPE)
+
+    def setUp(self):
+        self.readerClass = JanusGraphSONReader()
+        self.reader = None
+
+        pass
+
+    def test_mock_deserializer(self):
+        deserializer = MockDeserializer
+
+        self.readerClass.register_deserializer(self.GeoShape_GRAPHSON_TYPE, deserializer)
+        self.reader = self.readerClass.build()
+
+        self.reader.toObject()
+        return
