56c236e9cfd7dd2ab8255ab70474cf5ad5250c39
Debasish Kanhar <dekanhar@in.ibm.com>
2018-08-20 00:19:30 +0530

    Added Liscence test, author, version, credits, and other project metadata like contacts.
    Signed-off-by: Debasish Kanhar <dekanhar@in.ibm.com>

--- src/main/python/__init__.py
+++ src/main/python/__init__.py
@@ -1,20 +1,22 @@
-'''
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
+"""
+Copyright 2018 Debasish Kanhar
 
-http://www.apache.org/licenses/LICENSE-2.0
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-'''
+    http://www.apache.org/licenses/LICENSE-2.0
 
-__author__ = 'Debasish Kanhar'
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+__author__ = "Debasish Kanhar (https://github.com/debasishdebs)"
+__credits__ = ["Florian Hockman", "Jason Plurad", "Dave Brown", "Marko Rodriguez"]
+__license__ = "Apache-2.0"
+__version__ = "0.0.1"
+__email__ = ["d.kanhar@gmail.com", "dekanhar@in.ibm.com"]
+
