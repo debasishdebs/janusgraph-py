667e3c20c295947af56e82bab6ce0ec2a99490a6
Debasish Kanhar <dekanhar@in.ibm.com>
2018-09-26 03:40:13 +0530

    Added class for IndexBuilder, CompositeIndexBuilder and MixedIndexBuilder. Updated Helpers to add awaitGraphIndex and updateGraphIndex methods.
    Signed-off-by: Debasish Kanhar <dekanhar@in.ibm.com>

--- src/main/python/janusgraph_python/core/schema/Helpers.py
+++ src/main/python/janusgraph_python/core/schema/Helpers.py
@@ -3,6 +3,8 @@
 
 
 class Helpers(object):
+    REPEAT_AWAIT_AFTER_INDEX = 3
+
     @staticmethod
     def open_graph_management():
         query = "mgmt = graph.openManagement();\n"
@@ -12,3 +14,13 @@ class Helpers(object):
     def close_graph_management():
         query = "mgmt.commit();\n"
         return query
+
+    @staticmethod
+    def awaitGraphIndexStatus(index_name):
+        q = "mgmt.awaitGraphIndexStatus(graph, '{}').status(SchemaStatus.REGISTERED).call();\n".format(index_name)
+        return q
+
+    @staticmethod
+    def updateIndex(index_name):
+        q = "mgmt.updateIndex(mgmt.getGraphIndex('{}'), SchemaAction.REINDEX).get();\n".format(index_name)
+        return q
---
+++ src/main/python/janusgraph_python/core/schema/index/CompositeIndexBuilder.py
@@ -0,0 +1,111 @@
+# Name: Debasish Kanhar
+
+from ..SchemaBuilder import SchemaBuilder
+from ..Helpers import Helpers as helpers
+
+
+class CompositeIndexBuilder(SchemaBuilder):
+    def __init__(self, connection, query, index_name):
+        """
+
+        Args:
+            connection:
+            query (str): The already build index query which contains metadata lise Index Name and Graph element
+        """
+
+        super(CompositeIndexBuilder, self).__init__(connection)
+
+        open_management = helpers.open_graph_management()
+        self.query = open_management + query
+
+        self.index_name = index_name
+
+        # Sanitation check params
+        self.keys_added = None
+        self.unique_count = 0
+        self.label_constraint = False
+        self.unique_constraint = False
+
+        pass
+
+    def addKey(self, property_name):
+
+        if not self.label_constraint:
+            self.keys_added = property_name
+
+            q = ".addKey({})".format(property_name)
+            self.query += q
+
+        else:
+            raise AttributeError("addKey() can't be invoked once indexOnly() is already called")
+
+        return self
+
+    def indexOnly(self, label):
+
+        if not self.unique_constraint:
+            self.label_constraint = True
+
+            q = ".indexOnly({})".format(label)
+            self.query += q
+        else:
+            raise AttributeError("indexOnly() can't be invoked once unique() is already called")
+
+        return self
+
+    def unique(self):
+
+        self.unique_constraint = True
+
+        if self.unique_count < 1:
+            q = ".unique()"
+            self.query += q
+        else:
+            raise AttributeError("unique() can only be called once. "
+                                 "Being called {} times already".format(self.unique_count))
+
+        self.unique_count += 1
+
+        return self
+
+    def make(self):
+        if self.keys_added is None:
+            raise AttributeError("addKey() needs to be called before make()")
+
+        q = ".buildCompositeIndex();"
+        self.query += q
+
+        self.query += helpers.close_graph_management()
+
+        self.create(self.query)
+
+        return self.index_name
+
+    def create(self, query):
+
+        i = helpers.REPEAT_AWAIT_AFTER_INDEX
+
+        while i:
+            q = helpers.open_graph_management()
+            query += q
+
+            q = helpers.awaitGraphIndexStatus(self.index_name)
+            query += q
+
+            q = helpers.close_graph_management()
+            query += q
+
+            i -= 1
+
+        q = helpers.open_graph_management()
+        query += q
+
+        q = helpers.updateIndex(self.index_name)
+        query += q
+
+        q = helpers.close_graph_management()
+        query += q
+
+        super().create(query)
+
+        return self.index_name
