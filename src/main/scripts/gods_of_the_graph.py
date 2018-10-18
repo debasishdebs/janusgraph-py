866c7b877cccbd318336dfa7c536253003e5f1ad
Debasish Kanhar <dekanhar@in.ibm.com>
2018-10-18 20:09:45 +0530

    Updated graph_of_the_gods script as sample to load schema and data.
    Signed-off-by: Debasish Kanhar <dekanhar@in.ibm.com>

--- src/main/scripts/gods_of_the_graph.py
+++ src/main/scripts/gods_of_the_graph.py
@@ -2,6 +2,7 @@
 # Emp ID: 05222V
 
 from janusgraph_python.structure.JanusGraph import JanusGraph
+from janusgraph_python.core.datatypes.GeoShape import GeoShape
 
 
 if __name__ == '__main__':
@@ -64,3 +65,65 @@ if __name__ == '__main__':
     print("Build vertex monster")
 
     graph.commit()
+    # Once committed, we can't use any of graph's methods and hence can't use traversal.
+    # We will need to create another JanusGraph object before continuing.
+
+    graph = JanusGraph().connect(url="13.71.86.240", port="8182", bindings="g", graph="graph")
+
+    # Adding Data now
+    g = graph.traversal()
+    print("Adding vertices")
+    # Vertices
+    saturn = g.addV("titan").property("name", "saturn").property("age", 10000).next()
+    sky = g.addV("location").property("name", "sky").next()
+    sea = g.addV("location").property("name", "sea").next()
+    jupiter = g.addV("god").property("name", "jupiter").property("age", 5000).next()
+    neptune = g.addV("god").property("name", "neptune").property("age", 4500).next()
+    hercules = g.addV("demigod").property("name", "hercules").property("age", 30).next()
+    alcmene = g.addV("human").property("name", "alcmene").property("age", 45).next()
+    pluto = g.addV("god").property("name", "pluto").property("age", 4000).next()
+    nemean = g.addV("monster").property("name", "nemean").next()
+    hydra = g.addV("monster").property("name", "hydra").next()
+    cerberus = g.addV("monster").property("name", "cerberus").next()
+    tartarus = g.addV("location").property("name", "tartarus").next()
+    print("Vertices added successfully")
+
+    print("Adding Edges")
+    # Edges
+    g.V(jupiter).as_("from").V(saturn).addE("father").from_("from").next()
+    # or
+    # g.V(saturn).as_("to").V(jupiter).addE("father").to("to").next()
+    g.V(jupiter).as_("from").V(sky).addE("lives").property("reason", "loves fresh breezes").from_("from").next()
+    g.V(jupiter).as_("from").V(neptune).addE("brother").from_("from").next()
+    g.V(jupiter).as_("from").V(pluto).addE("brother").from_("from").next()
+
+    g.V(neptune).as_("from").V(sea).addE("lives").property("reason", "loves waves").from_("from").next()
+    g.V(neptune).as_("from").V(jupiter).addE("brother").from_("from").next()
+    g.V(neptune).as_("from").V(pluto).addE("brother").from_("from").next()
+
+    g.V(hercules).as_("from").V(jupiter).addE("father").from_("from").next()
+    g.V(alcmene).as_("from").V(hercules).addE("mother").from_("from").next()
+    pt1 = GeoShape.Point(38.1, 23.7)
+    g.V(hercules).as_("from").V(nemean).addE("battled").property("time", 1).property("place", pt1).next()
+    pt1 = GeoShape.Point(37.7, 23.9)
+    g.V(hercules).as_("from").V(hydra).addE("battled").property("time", 2).property("place", pt1).next()
+    pt1 = GeoShape.Point(39, 22)
+    g.V(hercules).as_("from").V(cerberus).addE("battled").property("time", 12).property("place", pt1).next()
+
+    g.V(pluto).as_("from").V(jupiter).addE("brother").from_("from").next()
+    g.V(pluto).as_("from").V(neptune).addE("brother").from_("from").next()
+    g.V(pluto).as_("from").V(tartarus).addE("lives").property("reason", "no fear of death").from_("from").next()
+    g.V(pluto).as_("from").V(cerberus).addE("pet").from_("from").next()
+
+    g.V(cerberus).as_("from").V(tartarus).addE("lives").from_("from").next()
+
+    print("Added Edges")
+
+    graph.commit()
+
+    graph = JanusGraph().connect(url="13.71.86.240", port="8182", bindings="g", graph="graph")
+    g = graph.traversal()
+
+    print("Counts")
+    print("Vertex: ", g.V().count().next())
+    print("Edge: ", g.E().count().next())
